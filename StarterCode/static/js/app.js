a4a2c5e273afc0268188ca14fabd4d67dc1ce7c3
Alexis Speliotis <akspeliotis@gmail.com>
2020-12-07 17:27:42 -0800

    Updates

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -10,7 +10,7 @@ function init() {
         });
         var firstSample = sampleNames[0];
         buildPlots(firstSample);
-
+        getdemoInfo(firstSample);
 
     });
 }
@@ -42,57 +42,60 @@ function buildPlots(sample) {
        }];
        Plotly.newPlot("bubble", bubbleChart_data, bubbleChart_layout);
 
-       var ids = samples.samples[0].otu_ids;
-       var sampleValues = samples.samples[0].sample_values.slice(0,10).reverse()
-       var labels = samples.samples[0].otu_labels.slice(0,10);
-       var otuTop = (samples.samples[0].otu_ids.slice(0,10)).reverse();
-       var otuId = otuTop.map(d => "OTU" + d);
-       var labels = samples.samples[0].otu_labels.slice(0,10);
+    //    var ids = samples.samples[0].otu_ids;
+    //    var sampleValues = samples.samples[0].sample_values.slice(0,10).reverse()
+    //    var labels = samples.samples[0].otu_labels.slice(0,10);
+    //    var otuTop = (samples.samples[0].otu_ids.slice(0,10)).reverse();
+    //    var otuId = otuTop.map(d => "OTU" + d);
+    //    var labels = samples.samples[0].otu_labels.slice(0,10);
 
        var barChart_trace = {
-           x: samples,
-           y: otuId,
-           text: labels,
-           marker: {
-               color: 'blue'},
-            type: "bar"
-            orientation: "h",
+           x: sample_values.slice(0,10).reverse(),
+           y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+           text: otu_labels.slice(0,10).reverse(),
+        //    marker: {
+        //        color: 'blue'},
+            type: "bar",
+            orientation: "h"
        };
 
        var barChart_data = [barChart_trace];
 
        var barChart_layout = {
            title: "Top 10 OTU",
-           yaxis:{
-               tickmode:"linear"},
+        //    yaxis:{
+        //        tickmode:"linear"},
             margin: {
-                l: 100,
-                r: 100,
-                t: 100,
-                b: 30
+                l: 150,
+                t: 30
             }   
        };
-       Plotly.newPlot("bar", data, layout);
+       Plotly.newPlot("bar", barChart_data, barChart_layout);
        
     });
 }
 function optionChanged(newSample) {
     buildPlots(newSample);
+    getdemoInfo(newSample);
+    
 }
 
-function get demoInfo(id) {
-    d3.json("samples.json").then((data=> {
+function getdemoInfo(id) {
+    d3.json("samples.json").then((data)=> {
         var metadata = data.metadata;
 
-        console.log(metadata)
+        console.log(metadata);
 
         var result = metadata.filter(meta => meta.id.toString() ===id)[0];
-        var demographicInfo = d3.select("sample-metadata")
+
+        console.log(result);
+
+        var demographicInfo = d3.select("#sample-metadata");
 
         demographicInfo.html("");
 
-        Object.defineProperties(result.forEach((key) => {
-            demographicInfo.append("h5".text(key[0].toUpperCase() + ": " + key[1] + "\n");)
+        Object.entries(result).forEach(([key,value]) => {
+            demographicInfo.append("h5").text(`${key.toUpperCase()}: ${value}`);
         });
     });
 }
